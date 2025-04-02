b20cc9a2e12c158490d75085cfa578505af9566b
rosa ovalle <you@example.com>
2025-04-02 13:14:10 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class EventRegistration : Controller
+    {
+        [Required]
+        public string FullName { get; set; }
+        [MinLength(3)]
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string EventCode { get; set; }
+        [MaxLength(20)]
+        public int Tickets { get; set; }
+        [Range(1,10)]
+
+        public string ReferralCode { get; set; }
+    }
+}
